c1379bbff6cce5ea6fa5c62ef8cf9f8dba627052
anthonymartin28 <77482894+anthonymartin28@users.noreply.github.com>
2021-11-16 13:51:11 +0100

    update interfaceController.java
    ajoute des objets de Arrivee pour connecter les bouton de l'interface avec le code de RunComplexe

--- cfun/src/main/java/org/openfx/cfun/interfaceController.java
+++ cfun/src/main/java/org/openfx/cfun/interfaceController.java
@@ -4,50 +4,55 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-public class interfaceController {
+public class interfaceController{
+
+	private static int nbMuscu = 4;
+	private static int nbFit = 5;
+	private static final String nomComplexe = "C Fun";
 	
-	
+	Complexe leComplexe = new Complexe(nbMuscu, nbFit, nomComplexe);
+	Arrivee ARV = new Arrivee(leComplexe,'F');
+
 	@FXML
 	private AnchorPane Accueil;
 
 	@FXML
 	private AnchorPane Sport;
-	    
+	
 	@FXML TextField txt_console;
 
 	@FXML
 	private AnchorPane yesno;
 
-    @FXML
-    private Button btn_entree;
+	@FXML
+    	private Button btn_entree;
 
-    @FXML
-    private Button btn_fitness;
+    	@FXML
+    	private Button btn_fitness;
 
-    @FXML
-    private Button btn_musculation;
+    	@FXML
+    	private Button btn_musculation;
 
-    @FXML
-    private Button btn_no;
+    	@FXML
+    	private Button btn_no;
 
-    @FXML
-    private Button btn_oui;
+    	@FXML
+    	private Button btn_oui;
 
-    @FXML
-    private Button btn_sortie;
+    	@FXML
+    	private Button btn_sortie;
     
   
     public void Entree() {
     	Accueil.setVisible(false);
     	Sport.setVisible(true);
-    	txt_console.setText("leComplexe.lesInfos()");
     }
     
     public void Sport() {
     	
     	Sport.setVisible(false);
     	yesno.setVisible(true);
-    	
+    	txt_console.setText(ARV.afficheBillet());
     }
     
   
