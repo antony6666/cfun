0526ab9cbb42f74bbff4a40ddb75077db87727c6
HugoGapaillart <77482647+HugoGapaillart@users.noreply.github.com>
2021-11-16 13:51:54 +0100

    Class ChoixMontant

---
+++ cdv/ChoixMontant.java
@@ -0,0 +1,17 @@
+package com.cdv;
+
+public class ChoixMontant {
+
+	public String ConstructeurChoixMontant(double a) {
+		
+		if (a <= 0.25) {
+			return "zero";
+		}else if (a > 0.25 && a < 0.5) {
+			return "zero cing";
+		}else if (a > 0.5 && a < 1) {
+			return "un";
+		}
+		return null;
+	}
+
+}
